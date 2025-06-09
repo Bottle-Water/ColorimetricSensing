3dc117eaaeb78f8194a42c7f4641f0e8ada9f401
ever4Ever11 <jiasiyjs@gmail.com>
2025-06-08 21:57:38 -0400

    type definitions

---
+++ types/data.ts
@@ -0,0 +1,41 @@
+export type Circle = {
+  x: number,
+  y: number,
+  r: number
+}
+
+
+export type Color = {
+  format: ColorFormat,
+  values: number[]
+}
+
+
+export type ColorFormat = 'rgb'
+
+
+export type Concentration = {
+  value: number,
+  units: Units
+}
+
+
+export type DataPoint = {
+  id: number,
+  image: string,
+  spots: Spot[],
+  concentration: Concentration | null
+}
+
+
+export type Spot = {
+  type: SpotType,
+  area: Circle,
+  color: Color
+}
+
+
+export type SpotType = 'reference' | 'sample';
+
+
+export type Units = 'ppm'
